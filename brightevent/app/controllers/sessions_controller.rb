6f7181c06a787e84c2ae448ba70de8e944c43808
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-10 10:44:04 -0700

    fixed sign in and out page as intended

--- brightevent/app/controllers/sessions_controller.rb
+++ brightevent/app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
     def new   
     end
-
     def create 
-        user = User.find_by_email params[:email] #
+        user = User.find_by_email params[:email]
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             flash[:success] = "Signed in!"
@@ -13,7 +12,6 @@ class SessionsController < ApplicationController
             render :new
           end
         end
-      
     def destroy 
         session[:user_id] = nil
         flash[:danger] = "Logged out!"
--- brightevent/app/controllers/users_controller.rb
+++ brightevent/app/controllers/users_controller.rb
@@ -2,16 +2,18 @@ class UsersController < ApplicationController
     def new 
         @user = User.new 
     end
-
     def create
-        @user = User.create params.require(:user).permit(
+        @user = User.new params.require(:user).permit(
             :first_name, 
             :last_name, 
             :email, 
             :password, 
-            :password_confirmation)
+            :password_confirmation
+        )
+            
+            
         if @user.save
-            session[:user_id] = @user.id # What does this one do? 
+            session[:user_id] = @user.id
             redirect_to root_path
         else 
             render 'new'
