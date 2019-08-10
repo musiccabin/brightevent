5cb87112b366b438b13451ced9201289ca18de07
Tyson Graham <tyson.gra@gmail.com>
2019-08-10 13:35:52 -0700

    finsihed up controller, have not tested yet

--- brightevent/app/controllers/rsvps_controller.rb
+++ brightevent/app/controllers/rsvps_controller.rb
@@ -1,15 +1,27 @@
 class RsvpsController < ApplicationController
 
   def create
+    event = Event.find params [:event_id]
+    rsvp = Rsvp.new user: current_user, event: event
+    # if !can?(:rsvp, event)
+    #   redirect_to event_path, alert: "you cant RSVP to this event"
+    # elsif rsvp.after_save 
+
+    if rsvp.save
+        redirect_to event.path, notice: " your day just got a bit brighter"
+    end
 
   end
 
 
   def destroy
-
+    event = Event.find params [:event_id]
+    rsvp = Rsvp.find params[:id]
+    @rsvp.destroy
+    redirect_to event_path
 
   end
-  
+
 
 
 end
