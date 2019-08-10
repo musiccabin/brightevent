e3d98d04ea0c53e9c8efd2e77a25bdd3a8563b1b
Tyson Graham <tyson.gra@gmail.com>
2019-08-10 12:14:23 -0700

    added more code to show index and create

--- brightevent/app/controllers/events_controller.rb
+++ brightevent/app/controllers/events_controller.rb
@@ -1,18 +1,24 @@
 class EventsController < ApplicationController
 
-
-
   def show
+    @event = Event.find(params[:id])
 
   end
 
   def index
+    @events = Event.all.order("created_at DESC")
 
-  end
+   end
 
 
-  def create
-
+   def create
+    @event = Event.new event_params
+    @event.user_id = current_user.id
+    if @event.save
+      redirect_to @event
+    else 
+      render :new
+    end
   end
 
 end
