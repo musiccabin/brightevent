073794fd4aaa8030caf3d0dccc4c474e9ec3af3c
yogiduzit <yverma2@my.bcit.ca>
2019-08-10 15:33:47 -0700

    v2.0

--- brightevent/app/controllers/events_controller.rb
+++ brightevent/app/controllers/events_controller.rb
@@ -1,14 +1,59 @@
 class EventsController < ApplicationController
 
-    def index
-    
-    end
+  include RemoteLinkPagination
 
-    def new
-    
+  before_action :authenticate_user!, except: [:show, :index, :new]
+
+  def new
+    @event = Event.new
+  end
+  
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to(event_path(@event))
+    else
+      render :edit
     end
-    
-    def show 
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  def show
+    @event = Event.find(params[:id])
+
+  end
+
+  def index
+    @events = Event.paginate(page: params[:page], per_page: 6).order(created_at: :desc)
+  end
+
+  def create
+    @event = Event.new event_params
+    @event.user_id = current_user.id
+    if @event.save
+      redirect_to @event
+    else 
+      render :new
     end
+  end
+
+  def link(text, target, attributes = {})
+    attributes['data-remote'] = true
+    super
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :date,:location, :image_url)
+  end
 
 end
--- brightevent/db/seeds.rb
+++ brightevent/db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+NUM_USERS = 4
+NUM_EVENTS = 20
+
+counter = 0
+
+NUM_USERS.times do 
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name}-#{last_name}-#{counter}@gmail.com",
+    password: "supersecret",
+    img_url: "https://i.pinimg.com/236x/e6/6f/14/e66f14e52252fc0eacbc28e54850b274.jpg"
+  )
+  counter += 1
+end
+
+NUM_EVENTS.times do 
+  Event.create(
+    user: User.all.sample,
+    title: Faker::Cannabis.buzzword,
+    description: Faker::ChuckNorris.fact,
+    where: "CF Pacific Center",
+    date: "11 August, 2019",
+    img_url: 'https://blogmedia.evbstatic.com/wp-content/uploads/wpmulti/sites/8/2018/01/15155312/iStock-667709450.jpg'
+
+  )
+end
+
+puts "Generated #{Event.count} events"
+puts "Generated #{User.count} users"
