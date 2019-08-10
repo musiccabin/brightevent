f8915b751ea3b788d3cfdb8adbf90f8b4800d857
dustinxie <dustinxie@hotmail.com>
2019-08-10 16:08:18 -0700

    user show page

--- brightevent/app/controllers/users_controller.rb
+++ brightevent/app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
         end
     end
     def show 
-        @user = User.find(user_params)
+       
+        @user = User.find(params[:id])
     end 
+    private
+    def user_params
+      params.require(:user).permit(:first_name,:last_name, :email)
+    end
 end
