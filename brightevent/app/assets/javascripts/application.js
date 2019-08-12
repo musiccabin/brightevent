d90ff892ff61db20290d20abfe77930f645ee439
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-11 18:09:57 -0700

    accepted fadeout jquery in application js

--- brightevent/app/assets/javascripts/application.js
+++ brightevent/app/assets/javascripts/application.js
@@ -18,40 +18,33 @@
 //= require activestorage
 //= require_tree .
 
-$('body').on('click', '[data-go-to-page]', function(){
-    var input_text = '<input type="text" name="go_to_page" size="4">';
-    var span = $(this).find('span');
-    span.html(input_text);
-    $("[name='go_to_page']").focus();
-  });
-  
-  $('body').on('keypress', 'input[name="go_to_page"]', function (e) {
-    new_page = $(this).val();
-    var charCode = (e.which) ? e.which : e.keyCode;
-    if (charCode == 13 && new_page > 0) {
-        $.ajax({
-            dataType: "script",
-            url: window.location.href,
-            data: {page: new_page}
-        });
-        $(this).parent().html('&hellip;');
-    }
-  });
-
-  $(document).ready(()=>{
-    
-    $('.carousel').carousel({
-      interval: 2000
-    })
-<<<<<<< HEAD
-    })
-=======
-    $(".flash" ).fadeOut(5000);
-
+$('body').on('click', '[data-go-to-page]', function () {
+  var input_text = '<input type="text" name="go_to_page" size="4">';
+  var span = $(this).find('span');
+  span.html(input_text);
+  $("[name='go_to_page']").focus();
 });
 
-  
-     
+$('body').on('keypress', 'input[name="go_to_page"]', function (e) {
+  new_page = $(this).val();
+  var charCode = (e.which) ? e.which : e.keyCode;
+  if (charCode == 13 && new_page > 0) {
+    $.ajax({
+      dataType: "script",
+      url: window.location.href,
+      data: {
+        page: new_page
+      }
+    });
+    $(this).parent().html('&hellip;');
+  }
+});
 
-  
->>>>>>> integration
+$(document).ready(() => {
+
+  $('.carousel').carousel({
+    interval: 2000
+  })
+  $(".flash").fadeOut(5000);
+
+});
